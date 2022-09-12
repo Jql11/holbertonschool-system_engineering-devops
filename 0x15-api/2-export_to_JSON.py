8fa08ff03a1f9f21ab1350bcf9be14c3a3c8b6a2
jql11 <4325@holbertonschool.com>
2022-09-12 04:14:13 -0700

    add file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+export to csv
+"""
+import csv
+import json
+import requests as req
+import sys
+
+if __name__ == "__main__":
+    USER_ID = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    response = req.get(url + 'users/{}'.format(USER_ID))
+    todo = req.get(url + 'todos?userId={}'.format(USER_ID))
+    USERNAME = response.json().get('username')
+
+    with open("{}.json".format(USER_ID), "w") as outfile:
+        tasks = []
+        for task in todo.json():
+            dictionary = {}
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            dictionary["task"] = "{}".format(TASK_TITLE)
+            dictionary["completed"] = "{}".format(TASK_COMPLETED_STATUS)
+            dictionary["username"] = "{}".format(USERNAME)
+            tasks.append(dictionary)
+        output = {
+                "{}".format(sys.argv[1]): "{}".format(tasks)
+        }
+        json.dump(output, outfile)
