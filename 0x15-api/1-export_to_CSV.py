628bf6fcf22f85970e0df96812bd9aceae22f449
jql11 <4325@holbertonschool.com>
2022-09-12 00:03:53 -0700

    add double string

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,9 +2,9 @@
 """
 export to csv
 """
+import csv
 import requests as req
 import sys
-import csv
 
 if __name__ == "__main__":
     USER_ID = sys.argv[1]
@@ -13,10 +13,9 @@ if __name__ == "__main__":
     todo = req.get(url + 'todos?userId={}'.format(USER_ID))
     USERNAME = response.json().get('username')
 
-    f = open('{}.csv'.format(USER_ID), 'w')
-    writer = csv.writer(f)
+    f = open('{}.csv'.format(USER_ID), 'wt')
+    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
     for task in todo.json():
         TASK_COMPLETED_STATUS = task.get('completed')
         TASK_TITLE = task.get('title')
         writer.writerow([USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
-    f.close()
