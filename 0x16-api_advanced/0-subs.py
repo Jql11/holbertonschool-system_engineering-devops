5cad58d765274df659e579c18028e0e5524f61a0
Jql11 <4325@holbertonschool.com>
2022-09-27 13:00:47 +1000

    fix style and invalid reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,8 @@ import requests
 def number_of_subscribers(subreddit):
     """number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers = {'User-agent': 'JQL 0x16'}).json()
-    subscribers = response.get('data').get('subscribers')
+    response = requests.get(url, headers={'User-agent': 'JQL 0x16'}).json()
+    subscribers = response.get('data', {}).get('subscribers')
     if not subscribers:
         return 0
-    return subscribers    
-	
+    return subscribers
