04efa14dcfe4fd63e3a7654d654eab3d09ad4f09
Jql11 <4325@holbertonschool.com>
2022-09-27 12:58:14 +1000

    add file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers = {'User-agent': 'JQL 0x16'}).json()
+    subscribers = response.get('data').get('subscribers')
+    if not subscribers:
+        return 0
+    return subscribers    
+	
